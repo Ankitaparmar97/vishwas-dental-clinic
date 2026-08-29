08462e6935282295d5461a42967e79a439fc7481
Ankita Parmar <ankitaparmar18697@gmail.com>
2026-08-29 18:22:06 +0530

    address change

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,10 +3,11 @@ export const CONTACT = {
   Mobile: '+918949706317',
   Whatsapp: '+919909508292',
   Address:
-    'C27, Vishwas Dental Clinic, Near lakhubhaihall, Opp. Om plaza, Kaliyabid, Bhavnagar-364001',
+    'Near bus stand, Arogya Nagar, Ganotri Society, Himatnagar, Gujarat 383001',
   AddressLink:
-    'https://maps.google.com/maps?q=Bhavnagar%20dental%20and%20implant%20hospital%20Lakhubha%20Hall%20Road%2C%20opposite%20Om%20Plaza%2C%20near%20Ram%20Mantra%20Mandir%2C%20Ramnagar%2C%20Kaliyabid%2C%20Bhavnagar%2C%20Gujarat%20364002&z=14&output=embed',
-  MapLink: 'https://maps.app.goo.gl/DBXhdfdETgPQchNH9',
+    'https://maps.google.com/maps?q=Vishwas%20Dental%20Clinic%2C%20Near%20bus%20stand%2C%20Arogya%20Nagar%2C%20Ganotri%20Society%2C%20Himatnagar%2C%20Gujarat%20383001&z=14&output=embed',
+  MapLink:
+    'https://www.google.com/search?sca_esv=99dc7e57846742f0&sxsrf=APpeQnueh9n8EWeV3KXPumrwRtG1G8gClg:1788007649551&q=vishwas+dental+clinic+himatnagar+address&ludocid=2028864876436992456&sa=X&ved=2ahUKEwjCo7nG78WWAxVYb2wGHXcKGVYQ6BN6BAg_EAI',
 };
 
 export const HOSPITAL_NAME = 'Vishwas Dental Clinic';
